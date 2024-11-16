938ab1c9c4c82d44050a3958b12db7da3928ae27
m13hack <119548847+m13hack@users.noreply.github.com>
2024-11-16 08:52:09 +0530

    Create stem_slicing.py

---
+++ stem_slicing.py
@@ -0,0 +1,8 @@
+greeting = "Hello"
+name = "Alice"
+message = greeting + ", " + name + "!"
+print(message)
+length = len(message)
+print("Length of message:", length)
+slice_message = message[0:5]
+print("Sliced message:", slice_message)
