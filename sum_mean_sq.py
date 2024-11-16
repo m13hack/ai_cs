dce31470987bd04b2665335b990c349cd39a395f
m13hack <119548847+m13hack@users.noreply.github.com>
2024-11-16 08:53:50 +0530

    Create sum_mean_sq.py

---
+++ sum_mean_sq.py
@@ -0,0 +1,8 @@
+import numpy as np
+array = np.array([1, 2, 3, 4, 5])
+array_sum = np.sum(array)
+array_mean = np.mean(array)
+array_squared = np.square(array)
+print("Sum of array:", array_sum)
+print("Mean of array:", array_mean)
+print("Squared array:", array_squared)
